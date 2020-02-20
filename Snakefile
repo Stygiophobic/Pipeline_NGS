7d484454d2b42962b98d7818be8d010d756daea0
Ext-Hadrien REGUE01 <regueex@chu-lyon.fr@di3224su.chu-lyon.fr>
2020-02-20 14:43:06 +0100

    maj

--- Snakefile
+++ Snakefile
@@ -16,12 +16,22 @@ SAMPLE_LIST=list(table_samplefile['SAMPLE'])
 
 
 rule all:
+    message:
+        "lolilolz, ca parse les fastq"  
     input:
         raw_fastq_R1 = expand(result_repository + fastq_repository_name + "/{sample}_R1.fastq.gz",sample=SAMPLE_LIST),
         raw_fastq_R2 = expand(result_repository + fastq_repository_name + "/{sample}_R2.fastq.gz",sample=SAMPLE_LIST)
-    output:
-    
-    
-    message:
-        "lolilolz, ca parse les fastq"  
+    #output:
+    run:
+        #Check number of sample in the fastq folder
+        SAMPLE_NUMBER=len(os.listdir(result_repository + fastq_repository_name))/2
+        SAMPLEFILE_NUMBER=len(SAMPLE_LIST)
+        if (SAMPLEFILE_NUMBER<SAMPLE_NUMBER):
+            print("WARNING:More samples in fastq directory than samplefile. Please verify the samplefile.\n")
+        if (SAMPLEFILE_NUMBER>SAMPLE_NUMBER):
+            print("WARNING:More samples in samplefile than fastq directory.Snakemake will stop.Please verify the samplefile.\n")
+
+        shell("echo 'LELZ'")
+
+
 
--- sandbox.sh
+++ sandbox.sh
@@ -3,8 +3,10 @@
 ################################################################################
 ##########################    CONFIGURATION    #################################
 ################################################################################
+heure=$(date +%H%M)
+jour=$(date +%Y%m%d) 
 singularity_img="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/IMG_SINGULARITY/PIPELINE-NGS.simg"
-fastq_repository="191217_NB501048_0665_AHWM32AFXY_1576689602"
+fastq_repository="200218_NB501048_0691_AHYTKHAFXY_1582192205"
 run_name="Test_OLD_DATA"
 working_repository="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/PIPELINE_NGS/"
 pathdata=$working_repository$fastq_repository
@@ -21,11 +23,13 @@ ln -s /srv/nfs/ngs-stockage/NGS_commun/disnap/NgsWeb/FastQ/${fastq_repository}/V
 ################################################################################
 #########################    LAUNCH SNAKEMAKE    ###############################
 ################################################################################ 
+#nohup 
 singularity exec $singularity_img snakemake \
-    --dryrun \
     --config Result_Repository=$working_repository \
              Project_folder=$fastq_repository \
-             Samplesheet_Location=$samplefile
+             Samplesheet_Location=$samplefile #\
+#    --dryrun \             
+#> $working_repository${rep_report}report_${jour}_${heure}.txt              
 ################################################################################
 ###option:
 # --dryrun => fait tourner le pipeline à vide pour controler
@@ -36,3 +40,7 @@ ln -s /srv/nfs/ngs-stockage/NGS_commun/disnap/NgsWeb/FastQ/${fastq_repository}/V
 
 #Remove symlink on data
 unlink $pathdata
+
+        """
+        echo 'LELZ'
+        """
