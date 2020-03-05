965a52bf4bac6faa5bf508add3809bb33716a437
Ext-Hadrien REGUE01 <regueex@chu-lyon.fr@di3193su.chu-lyon.fr>
2020-03-05 16:26:35 +0100

    test cmd

--- sandbox.sh
+++ sandbox.sh
@@ -5,7 +5,7 @@
 ################################################################################
 heure=$(date +%H%M)
 jour=$(date +%Y%m%d) 
-singularity_img="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/PIPELINE_NGS/NGS-PIPELINE.simg"
+singularity_img="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/PIPELINE_NGS/pipeline.simg"
 fastq_repository="200218_NB501048_0691_AHYTKHAFXY_1582192205"
 run_name="Test_OLD_DATA"
 working_repository="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/PIPELINE_NGS/"
@@ -47,3 +47,7 @@ bwa index -p temp/influenza_subtype mapping/pre_mapping/influenza_subtype.fasta
 #SAMTOOLS
 samtools view -bSc /srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/PIPELINE_NGS/SAM/S20119_S50.sam > /srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/PIPELINE_NGS/SAM/test.bam
 samtools view -S /srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/PIPELINE_NGS/SAM/S20119_S50.sam | cut -f 3 | sort | uniq -c | sort -nr | sed -e 's/^ *//;s/ /\t/' | tr '\t'  ';'> RefsReadsCount.txt
+
+
+samtools mpileup -u -d 1000 -f mapping/subtype_mapping/BVIC_Malaysia2506.fasta /srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/PIPELINE_NGS/BAM_SUBTYPE/S20119_S50.bam \
+| bcftools call --ploidy 1 -c | vcfutils.pl vcf2fq   > mdr.txt
